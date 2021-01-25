0fc2650b72119c83ad83b9b454825d15629e2589
ryogabarbie <ryoga1226@gmail.com>
2021-01-25 19:42:02 +0900

    SwiftPMでAPIKitを導入

--- App2/App2/ViewController.swift
+++ App2/App2/ViewController.swift
@@ -26,6 +26,8 @@ class ViewController: UIViewController {
                 print(string)
             })
             .dispose()
+
+        print(APIKit.Session.self)
     }
 
 
--- Statics/Static1/Static1/Static1.swift
+++ Statics/Static1/Static1/Static1.swift
@@ -8,3 +8,5 @@
 class Static1 {
 
 }
+
+import Umbrella2
